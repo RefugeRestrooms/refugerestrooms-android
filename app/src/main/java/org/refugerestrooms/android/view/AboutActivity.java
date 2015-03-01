f0d6be6203908b031b69e79cfaed1e2bea2c9eeb
Naimah Mumin <naimahmumin@gmail.com>
2015-02-28 18:26:45 -0800

    about activity

---
+++ app/src/main/java/org/refugerestrooms/android/view/AboutActivity.java
@@ -0,0 +1,18 @@
+package org.refugerestrooms.android.view;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.jmpumphrey.refugerestrooms.R;
+
+/**
+ * Created by naimahmumin on 2/28/15.
+ */
+public class AboutActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
