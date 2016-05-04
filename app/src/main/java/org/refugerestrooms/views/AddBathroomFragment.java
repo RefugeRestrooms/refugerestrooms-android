8e5133b00dea0091e3e8a3c95b27c62a890a8645
David Mascharka <davidmascharka@gmail.com>
2016-05-03 20:54:19 -0500

    Add layout for a local form to add restroom

--- app/src/main/java/org/refugerestrooms/views/AddBathroomFragment.java
+++ app/src/main/java/org/refugerestrooms/views/AddBathroomFragment.java
@@ -9,6 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import org.refugerestrooms.R;
 
@@ -23,6 +26,35 @@ public class AddBathroomFragment extends Fragment {
         mWebView.loadUrl("http://www.refugerestrooms.org/restrooms/new?");
         // Inflate the layout for this fragment
         return rootView;
+
+        /*View view = inflater.inflate(R.layout.fragment_add_bathroom, container, false);
+
+        Spinner countries = (Spinner) view.findViewById(R.id.add_bathroom_country);
+        ArrayAdapter<CharSequence> countryAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.countries, android.R.layout.simple_spinner_dropdown_item);
+        countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        countries.setAdapter(countryAdapter);
+
+        Spinner accessible = (Spinner) view.findViewById(R.id.add_bathroom_accessible);
+        ArrayAdapter<CharSequence> accessibleAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.accessibility, android.R.layout.simple_spinner_dropdown_item);
+        accessibleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        accessible.setAdapter(accessibleAdapter);
+
+        Spinner unisex = (Spinner) view.findViewById(R.id.add_bathroom_unisex);
+        ArrayAdapter<CharSequence> unisexAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.unisex, android.R.layout.simple_spinner_dropdown_item);
+        unisexAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        unisex.setAdapter(unisexAdapter);
+
+        ((Button) view.findViewById(R.id.add_bathroom_submit)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO submit a bathroom
+
+            }
+        });
+
+        return view;*/
     }
 }
-
