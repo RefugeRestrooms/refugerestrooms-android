ae9d2776123a09a3401a6428ef9ac9b5be6d103a
David Mascharka <davidmascharka@gmail.com>
2016-05-04 22:26:30 -0500

    Restore WebView state to preserve contents on orientation change

--- app/src/main/java/org/refugerestrooms/views/AddBathroomFragment.java
+++ app/src/main/java/org/refugerestrooms/views/AddBathroomFragment.java
@@ -9,52 +9,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
 
 import org.refugerestrooms.R;
 
 public class AddBathroomFragment extends Fragment {
+
+    private WebView mWebView;
+    private Bundle mWebViewBundle;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.add_bathroom, container, false);
-        WebView mWebView = (WebView) rootView.findViewById(R.id.addBathroom);
+        mWebView = (WebView) rootView.findViewById(R.id.addBathroom);
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new AddBathroomClient("http://www.refugerestrooms.org/restrooms/new?"));
-        mWebView.loadUrl("http://www.refugerestrooms.org/restrooms/new?");
+
+        // If possible, restore the WebView state - otherwise load the new restroom page
+        if (mWebViewBundle != null) {
+            mWebView.restoreState(mWebViewBundle);
+        } else {
+            mWebView.loadUrl("http://www.refugerestrooms.org/restrooms/new?");
+        }
+
         // Inflate the layout for this fragment
         return rootView;
+    }
 
-        /*View view = inflater.inflate(R.layout.fragment_add_bathroom, container, false);
-
-        Spinner countries = (Spinner) view.findViewById(R.id.add_bathroom_country);
-        ArrayAdapter<CharSequence> countryAdapter = ArrayAdapter.createFromResource(getContext(),
-                R.array.countries, android.R.layout.simple_spinner_dropdown_item);
-        countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        countries.setAdapter(countryAdapter);
-
-        Spinner accessible = (Spinner) view.findViewById(R.id.add_bathroom_accessible);
-        ArrayAdapter<CharSequence> accessibleAdapter = ArrayAdapter.createFromResource(getContext(),
-                R.array.accessibility, android.R.layout.simple_spinner_dropdown_item);
-        accessibleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        accessible.setAdapter(accessibleAdapter);
-
-        Spinner unisex = (Spinner) view.findViewById(R.id.add_bathroom_unisex);
-        ArrayAdapter<CharSequence> unisexAdapter = ArrayAdapter.createFromResource(getContext(),
-                R.array.unisex, android.R.layout.simple_spinner_dropdown_item);
-        unisexAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        unisex.setAdapter(unisexAdapter);
-
-        ((Button) view.findViewById(R.id.add_bathroom_submit)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO submit a bathroom
-
-            }
-        });
-
-        return view;*/
+    /**
+     * Save the WebView's state when the application pauses (e.g. on an orientation change)
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+        mWebViewBundle = new Bundle();
+        mWebView.saveState(mWebViewBundle);
     }
 }
